8f562e8a1f73ce073906f8a18d302de571fbd786
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-10-13 17:08:06 +0300

    Philately

---
+++ src/main/java/com/philately/model/entity/User.java
@@ -0,0 +1,44 @@
+package com.philately.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    @Length(min = 3, max = 20)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private Set<Stamp> purchasedStamps;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    private Set<Stamp> wishedStamps;
+
+}
