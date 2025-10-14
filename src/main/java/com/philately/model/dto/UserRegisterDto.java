90f0d3416f4f732cfff30a12ab1854e88f63c12c
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-10-14 17:03:24 +0300

    Philately

---
+++ src/main/java/com/philately/model/dto/UserRegisterDto.java
@@ -0,0 +1,29 @@
+package com.philately.model.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegisterDto {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String password;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String confirmPassword;
+
+}
