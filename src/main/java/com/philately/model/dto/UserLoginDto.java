6423a81b443c9d43163a9e5fa00f0980b6c1984f
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-10-14 17:03:12 +0300

    Philately

---
+++ src/main/java/com/philately/model/dto/UserLoginDto.java
@@ -0,0 +1,17 @@
+package com.philately.model.dto;
+
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserLoginDto {
+
+    @Size(min = 3, max = 20, message = "Username length must be between 3 and 20 characters!")
+    private String username;
+
+    @Size(min = 3, max = 20, message = "Password length must be between 3 and 20 characters!")
+    private String password;
+
+}
